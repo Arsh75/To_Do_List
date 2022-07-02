caa5e8fefe4731d8f6f99606d4169bf068e527a2
Arshad Khan <khan.arshad3008@gmail.com>
2022-07-02 21:56:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import Header from "./Mycomponents/Header";
+import { Todos } from "./Mycomponents/Todos";
+import { Footer } from "./Mycomponents/Footer";
+import { AddTodo } from "./Mycomponents/AddTodo";
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = []
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    //Deleting this way does not work in react 
+    // let index=todos.indexOf(todo);
+    // todos.splice(index,1);
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    localStorage.getItem("todos");
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding This Todo", title, desc)
+    let sno;
+    if (todos.length == 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+
+  }
+
+  const [todos, setTodos] = useState([initTodo]);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+  return (
+    <>
+
+      <Header title="My Todos List" searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
